f89e3c8ea7ffac9dafe4ea948e384ad0fb426557
Bobby Vasquez <shootbv@gmail.com>
2021-08-04 13:00:49 -0600

    Create basic functionality to render random tarot card

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,56 @@
 // import logo from './logo.svg';
+import React, { useState, useEffect }  from "react"
+import { getData } from '../../utilities/apiCalls'
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [cards, setCards] = useState([])
+  const [cryptoData, setCryptoData] = useState([])
+  const [results, setResults] = useState([])
+  const [randomCard, setRandomCard] = useState('')
+
+  useEffect(() => {
+    getData()
+    .then(data => {
+      setCards(data[0].cards)
+      setCryptoData(data[1].slice(0, 100)) // Top 100 Cryptocurrencies
+      setResults(data[2].results)
+    })
+  }, [])
+
+  const getRandomIndex = (array) => {
+    return Math.floor(Math.random() * array.length)
+  }
+
+  const getRandomCard = () => {
+    let random = cards[getRandomIndex(cards)]
+    setRandomCard(random)
+
+  }
+
+  const renderCard = () => {
+    if (!randomCard) {
+      getRandomCard()
+    }
+    return (
+      <>
+        <h2>{randomCard.name}</h2>
+        <h3>{randomCard.value}</h3>
+        <h3>{randomCard.description}</h3>
+      </>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+    {!!cards.length &&
+      <div>
+        <div>{renderCard()}</div>
+        <button onClick={getRandomCard}>New Card</button>
+      </div>
+    }
+    </main>
   );
 }
 
