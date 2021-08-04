d6b271fd29fbc2a03452d38a3489f01e95fa7baf
Bobby Vasquez <shootbv@gmail.com>
2021-08-04 14:08:51 -0600

    Add functionality to match results with random crypto and random tarot

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,15 +10,20 @@ const App = () => {
   const [results, setResults] = useState([])
   const [randomCard, setRandomCard] = useState('')
   const [randomCrypto, setRandomCrypto] = useState('')
+  const [randomCrypto2, setRandomCrypto2] = useState('')
   const [currentResult, setCurrentResult] = useState('')
 
   useEffect(() => {
+    let isMounted = true
     getData()
     .then(data => {
-      setCards(data[0].cards)
-      setCryptoData(data[1].slice(0, 100)) // Top 100 Cryptocurrencies
-      setResults(data[2].results)
+      if (isMounted) {
+        setCards(data[0].cards)
+        setCryptoData(data[1].slice(0, 100)) // Top 100 Cryptocurrencies
+        setResults(data[2].results)
+      }
     })
+    return () => isMounted = false;
   }, [])
 
   const getRandomIndex = (array) => {
@@ -31,13 +36,19 @@ const App = () => {
   }
 
   const getRandomCrypto = () => {
-    let random = cryptoData[getRandomIndex(cryptoData)]
-    setRandomCrypto(random)
+    let random1 = cryptoData[getRandomIndex(cryptoData)]
+    let random2 = cryptoData[getRandomIndex(cryptoData)]
+    if (random1 === random2) {
+      random2 = cryptoData[getRandomIndex(cryptoData)]
+    }
+    setRandomCrypto(random1)
+    setRandomCrypto2(random2)
   }
 
   const newRandoms = () => {
     getRandomCard()
     getRandomCrypto()
+    matchResults()
   }
 
   // const getRandomFrom = (dataset) => {
@@ -48,6 +59,17 @@ const App = () => {
   //   return setRandomCrypto(getRandomFrom(cryptoData))
   // }
 
+
+  const matchResults = () => {
+    let matching = results.filter(result => result.word_association.some(keyword => randomCard.keywords.includes(keyword)))
+    console.log(matching)
+    let relevant = matching[getRandomIndex(matching)]
+    const { result_choices } = relevant
+    let randomResult = result_choices[getRandomIndex(result_choices)]
+    setCurrentResult(randomResult)
+    // setCurrentResult(relevant.result_choices[getRandomIndex(relevant.result_choices)])
+  }
+
   const renderCrypto = () => {
     if (!randomCrypto) {
       getRandomCrypto()
@@ -57,6 +79,33 @@ const App = () => {
       <>
         <h2>{randomCrypto.name}</h2>
       </>
+    )
+  }
+
+  const readingResults = () => {
+    if (!currentResult) {
+      matchResults()
+    }
+
+    let injectResult = ''
+    if (currentResult.includes('[crypto2]')) {
+      injectResult = currentResult.replace('[crypto1]', randomCrypto.name)
+      injectResult = injectResult.replace('[crypto2]', randomCrypto2.name)
+      injectResult = injectResult.replace('[keyword1]', randomCard.keywords[0])
+      injectResult = injectResult.replace('[keyword2]', randomCard.keywords[1])
+    }
+    else {
+      injectResult = currentResult.replace('[crypto1]', randomCrypto.name)
+      injectResult = injectResult.replace('[keyword1]', randomCard.keywords[0])
+    }
+
+    injectResult = injectResult.replace(/\’\’/g, '\'')
+    injectResult = injectResult.replace(/\’\’/g, '\'')
+
+    return (
+      <div>
+      <h2>{injectResult}</h2>
+      </div>
     )
   }
 
@@ -76,12 +125,13 @@ const App = () => {
 
   return (
     <main className="App">
-    {!!cards.length && !!cryptoData.length &&
-      <div>
+    {!!cards.length && !!cryptoData.length && !!results.length &&
+      <section>
         <div>{renderCard()}</div>
         <div>{renderCrypto()}</div>
         <button onClick={newRandoms}>New Card</button>
-      </div>
+        <div>{!!randomCard && !!randomCrypto && <h3>{readingResults()}</h3>}</div>
+      </section>
     }
     </main>
   );
