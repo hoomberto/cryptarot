b09375456ea17918f54de927ee79dd030d7c23dd
Bobby Vasquez <shootbv@gmail.com>
2021-08-04 14:38:07 -0600

    Add file to handle imports/exports of images

---
+++ src/utilities/images.js
@@ -0,0 +1,184 @@
+// Reversed
+
+import reversed from '../../assets/images/reversed.jpg';
+
+//  Major Arcana
+
+import ar00 from '../../assets/images/ar00.jpg';
+import ar01 from '../../assets/images/ar01.jpg';
+import ar02 from '../../assets/images/ar02.jpg';
+import ar03 from '../../assets/images/ar03.jpg';
+import ar04 from '../../assets/images/ar04.jpg';
+import ar05 from '../../assets/images/ar05.jpg';
+import ar06 from '../../assets/images/ar06.jpg';
+import ar07 from '../../assets/images/ar07.jpg';
+import ar08 from '../../assets/images/ar08.jpg';
+import ar09 from '../../assets/images/ar09.jpg';
+import ar10 from '../../assets/images/ar10.jpg';
+import ar11 from '../../assets/images/ar11.jpg';
+import ar12 from '../../assets/images/ar12.jpg';
+import ar13 from '../../assets/images/ar13.jpg';
+import ar14 from '../../assets/images/ar14.jpg';
+import ar15 from '../../assets/images/ar15.jpg';
+import ar16 from '../../assets/images/ar16.jpg';
+import ar17 from '../../assets/images/ar17.jpg';
+import ar18 from '../../assets/images/ar18.jpg';
+import ar19 from '../../assets/images/ar19.jpg';
+import ar20 from '../../assets/images/ar20.jpg';
+import ar21 from '../../assets/images/ar21.jpg';
+
+// Minor Arcana
+
+// Wands
+
+import wa00 from '../../assets/images/wa00.jpg';
+import wa01 from '../../assets/images/wa01.jpg';
+import wa02 from '../../assets/images/wa02.jpg';
+import wa03 from '../../assets/images/wa03.jpg';
+import wa04 from '../../assets/images/wa04.jpg';
+import wa05 from '../../assets/images/wa05.jpg';
+import wa06 from '../../assets/images/wa06.jpg';
+import wa07 from '../../assets/images/wa07.jpg';
+import wa08 from '../../assets/images/wa08.jpg';
+import wa09 from '../../assets/images/wa09.jpg';
+import wa10 from '../../assets/images/wa10.jpg';
+import wa11 from '../../assets/images/wa11.jpg';
+import wa12 from '../../assets/images/wa12.jpg';
+import wa13 from '../../assets/images/wa13.jpg';
+import wa14 from '../../assets/images/wa14.jpg';
+
+// Cups
+
+import cu00 from '../../assets/images/cu00.jpg';
+import cu01 from '../../assets/images/cu01.jpg';
+import cu02 from '../../assets/images/cu02.jpg';
+import cu03 from '../../assets/images/cu03.jpg';
+import cu04 from '../../assets/images/cu04.jpg';
+import cu05 from '../../assets/images/cu05.jpg';
+import cu06 from '../../assets/images/cu06.jpg';
+import cu07 from '../../assets/images/cu07.jpg';
+import cu08 from '../../assets/images/cu08.jpg';
+import cu09 from '../../assets/images/cu09.jpg';
+import cu10 from '../../assets/images/cu10.jpg';
+import cu11 from '../../assets/images/cu11.jpg';
+import cu12 from '../../assets/images/cu12.jpg';
+import cu13 from '../../assets/images/cu13.jpg';
+import cu14 from '../../assets/images/cu14.jpg';
+
+// Swords
+
+import sw00 from '../../assets/images/sw00.jpg';
+import sw01 from '../../assets/images/sw01.jpg';
+import sw02 from '../../assets/images/sw02.jpg';
+import sw03 from '../../assets/images/sw03.jpg';
+import sw04 from '../../assets/images/sw04.jpg';
+import sw05 from '../../assets/images/sw05.jpg';
+import sw06 from '../../assets/images/sw06.jpg';
+import sw07 from '../../assets/images/sw07.jpg';
+import sw08 from '../../assets/images/sw08.jpg';
+import sw09 from '../../assets/images/sw09.jpg';
+import sw10 from '../../assets/images/sw10.jpg';
+import sw11 from '../../assets/images/sw11.jpg';
+import sw12 from '../../assets/images/sw12.jpg';
+import sw13 from '../../assets/images/sw13.jpg';
+import sw14 from '../../assets/images/sw14.jpg';
+
+// Swords
+
+import di00 from '../../assets/images/di00.jpg';
+import di01 from '../../assets/images/di01.jpg';
+import di02 from '../../assets/images/di02.jpg';
+import di03 from '../../assets/images/di03.jpg';
+import di04 from '../../assets/images/di04.jpg';
+import di05 from '../../assets/images/di05.jpg';
+import di06 from '../../assets/images/di06.jpg';
+import di07 from '../../assets/images/di07.jpg';
+import di08 from '../../assets/images/di08.jpg';
+import di09 from '../../assets/images/di09.jpg';
+import di10 from '../../assets/images/di10.jpg';
+import di11 from '../../assets/images/di11.jpg';
+import di12 from '../../assets/images/di12.jpg';
+import di13 from '../../assets/images/di13.jpg';
+import di14 from '../../assets/images/di14.jpg';
+
+export default [
+    reversed,
+    ar00,
+    ar01,
+    ar02,
+    ar03,
+    ar04,
+    ar05,
+    ar06,
+    ar07,
+    ar08,
+    ar09,
+    ar10,
+    ar11,
+    ar12,
+    ar13,
+    ar14,
+    ar15,
+    ar16,
+    ar17,
+    ar18,
+    ar19,
+    ar20,
+    ar21,
+    wa01,
+    wa02,
+    wa03,
+    wa04,
+    wa05,
+    wa06,
+    wa07,
+    wa08,
+    wa09,
+    wa10,
+    wa11,
+    wa12,
+    wa13,
+    wa14,
+    cu01,
+    cu02,
+    cu03,
+    cu04,
+    cu05,
+    cu06,
+    cu07,
+    cu08,
+    cu09,
+    cu10,
+    cu11,
+    cu12,
+    cu13,
+    cu14,
+    sw01,
+    sw02,
+    sw03,
+    sw04,
+    sw05,
+    sw06,
+    sw07,
+    sw08,
+    sw09,
+    sw10,
+    sw11,
+    sw12,
+    sw13,
+    sw14,
+    di01,
+    di02,
+    di03,
+    di04,
+    di05,
+    di06,
+    di07,
+    di08,
+    di09,
+    di10,
+    di11,
+    di12,
+    di13,
+    di14,
+];
