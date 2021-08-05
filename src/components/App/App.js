d43bd53368db0408d8fa81ac1a61353521b8d299
Bobby Vasquez <shootbv@gmail.com>
2021-08-04 23:24:30 -0600

    Add useEffect to set data on mounting

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,9 +6,30 @@ import CryptoHeader from '../CryptoHeader/CryptoHeader'
 import './App.css';
 
 const App = () => {
+
+  const [tarotData, setTarotData] = useState('')
+
+  useEffect(() => {
+  let isMounted = true
+  getData()
+  .then(data => {
+    if (isMounted) {
+      setTarotData({
+        cards: data[0].cards,
+        cryptoData: data[1].slice(0, 100),
+        results: data[2].results,
+        currentCard: getRandomCard(data[0].cards),
+        crypto: getRandomCrypto(data[1].slice(0, 100))
+      })
+    })
+  return () => {
+    isMounted = false
+  };
+}, [])
+
   return (
     <div className="App">
-
+      
     </div>
   );
 }
