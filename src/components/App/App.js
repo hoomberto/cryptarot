ce3a0872e01c5dc656770357c4af8aa6b3b98c0a
Bobby Vasquez <shootbv@gmail.com>
2021-08-05 09:25:24 -0600

    Move Card components and logic from app to container Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,56 +1,11 @@
-import React, { useState, useEffect }  from "react"
-import {
-  Route,
-  Link
-} from 'react-router-dom'
-import { getData } from '../../utilities/apiCalls'
-import { getRandomCard, getRandomCrypto } from '../../utilities/utils'
-import Card from '../Card/Card'
-import CardInfo from '../CardInfo/CardInfo'
-import CryptoHeader from '../CryptoHeader/CryptoHeader'
+import { Route } from 'react-router-dom'
+import CardChoice from '../CardChoice/CardChoice'
 import './App.css';
 
 const App = () => {
-
-  const [tarotData, setTarotData] = useState('')
-  const [reading, setReading] = useState('')
-
-  useEffect(() => {
-  let isMounted = true
-  getData()
-  .then(data => {
-    if (isMounted) {
-      setTarotData({
-        cards: data[0].cards,
-        cryptoData: data[1].slice(0, 100),
-        results: data[2].results,
-        currentCard: getRandomCard(data[0].cards),
-        crypto: getRandomCrypto(data[1].slice(0, 100))
-        })
-      }
-    })
-    return () => {
-      isMounted = false
-    };
-  }, [])
-
-  const newReading = () => {
-    setReading({
-      card: getRandomCard(tarotData.cards),
-      crypto: getRandomCrypto(tarotData.cryptoData)
-    })
-  }
-
   return (
     <main>
-      {!!tarotData &&
-        <section>
-          <CardInfo card={!!reading ? reading.card : tarotData.currentCard} />
-          <CryptoHeader crypto={!!reading ? reading.crypto : tarotData.crypto} />
-          <Card name={!!reading ? reading.card.name_short : tarotData.currentCard.name_short} />
-          <button onClick={newReading}>New Reading</button>
-        </section>
-      }
+      <Route exact to="/" component={CardChoice} />
     </main>
   );
 }
---
+++ src/components/CardChoice/CardChoice.js
@@ -0,0 +1,54 @@
+import React , {useState, useEffect} from 'react'
+import Card from '../Card/Card'
+import CardInfo from '../CardInfo/CardInfo'
+import CryptoHeader from '../CryptoHeader/CryptoHeader'
+import { getData } from '../../utilities/apiCalls'
+import { getRandomCard, getRandomCrypto } from '../../utilities/utils'
+import './CardChoice.css'
+
+const CardChoice = () => {
+
+  const [reading, setReading] = useState('')
+  const [tarotData, setTarotData] = useState('')
+
+  useEffect(() => {
+  let isMounted = true
+  getData()
+  .then(data => {
+    if (isMounted) {
+      setTarotData({
+        cards: data[0].cards,
+        cryptoData: data[1].slice(0, 100),
+        results: data[2].results,
+        currentCard: getRandomCard(data[0].cards),
+        crypto: getRandomCrypto(data[1].slice(0, 100))
+        })
+      }
+    })
+    return () => {
+      isMounted = false
+    };
+  }, [])
+
+  const newReading = () => {
+    setReading({
+      card: getRandomCard(tarotData.cards),
+      crypto: getRandomCrypto(tarotData.cryptoData)
+    })
+  }
+
+  return  (
+    <>
+      {!tarotData ? <h2>Loading...</h2> :
+        <section>
+          <CardInfo card={!!reading ? reading.card : tarotData.currentCard} />
+          <CryptoHeader crypto={!!reading ? reading.crypto : tarotData.crypto} />
+          <Card name={!!reading ? reading.card.name_short : tarotData.currentCard.name_short} />
+          <button onClick={newReading}>New Reading</button>
+        </section>
+      }
+    </>
+  )
+}
+
+export default CardChoice
