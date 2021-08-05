db8742676b1c6ad34a24bca2e2befc706245411a
Bobby Vasquez <shootbv@gmail.com>
2021-08-05 00:37:40 -0600

    Add better spacing to CryptoHeader render

--- src/components/CryptoHeader/CryptoHeader.js
+++ src/components/CryptoHeader/CryptoHeader.js
@@ -12,7 +12,7 @@ const oneCrypto = () => {
   // let twoCryptos = `${crypto[0].name} (${crypto[0].symbol}) & ${crypto[1].name} (${crypto[1].symbol})`;
   const twoCryptos = () => {
     return (
-      <div>
+      <div className="two-cryptos-container">
         <div className="crypto-header-container"><img className="logo" src={crypto[0].logo_url}/><h2>{crypto[0].name} <strong>({crypto[0].symbol})</strong></h2></div>
         <div className="crypto-header-container"><img className="logo" src={crypto[1].logo_url}/><h2>{crypto[1].name} <strong>({crypto[1].symbol})</strong></h2></div>
       </div>
