f94aeb84caf2fbc74053c81858f202f654909380
Bobby Vasquez <shootbv@gmail.com>
2021-08-05 15:38:22 -0600

    Create basic structure for  table row

---
+++ src/components/TableRow/TableRow.js
@@ -0,0 +1,13 @@
+const TableRow = (data) => {
+  return (
+    <tr>
+      <td>1</td>
+      <td>2</td>
+      <td>3</td>
+      <td>4</td>
+      <td>5</td>
+    </tr>
+  )
+}
+
+export default TableRow
