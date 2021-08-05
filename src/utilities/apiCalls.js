ba60598a85dea1e89d594ae7301ceeacaf23c98e
Bobby Vasquez <shootbv@gmail.com>
2021-08-04 23:10:15 -0600

    Add api calls

---
+++ src/utilities/apiCalls.js
@@ -0,0 +1,30 @@
+const nomics = "https://api.nomics.com/v1/currencies/ticker?key=39882e2a6e59afede70e88be7f53e7d7ed14b4c0"
+
+const getCards = () => {
+  return fetch('https://cryptarot-api.herokuapp.com/api/v1/cards')
+    .then(checkResponse)
+}
+
+const getCryptoData = () => {
+  return fetch(nomics)
+  .then(checkResponse)
+}
+
+const getResults = () => {
+  return fetch('https://cryptarot-api.herokuapp.com/api/v1/results')
+  .then(checkResponse)
+}
+
+const checkResponse = (response) => {
+  if (response.ok) {
+    return response.json();
+  } else {
+    return `Request could not go through.`
+  }
+};
+
+const getData = () => {
+  return Promise.all([getCards(), getCryptoData(), getResults()])
+}
+
+export { getData }
