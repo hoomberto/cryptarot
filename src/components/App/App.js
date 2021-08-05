26015357071cc299139a835e2c701b5f633ae42a
Bobby Vasquez <shootbv@gmail.com>
2021-08-04 23:21:06 -0600

    Import components to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,14 @@
-
+import React, { useState, useEffect }  from "react"
+import { getData } from '../../utilities/apiCalls'
+import Card from '../Card/Card'
+import CardInfo from '../CardInfo/CardInfo'
+import CryptoHeader from '../CryptoHeader/CryptoHeader'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
