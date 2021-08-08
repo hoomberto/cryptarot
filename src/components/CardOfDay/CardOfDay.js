9c3df6a25fa1477f126863ff8e31c40db3fb7c04
Bobby Vasquez <shootbv@gmail.com>
2021-08-07 23:48:34 -0600

    Fix styling across medias

--- src/components/CardOfDay/CardOfDay.js
+++ src/components/CardOfDay/CardOfDay.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Loading from '../Loading/Loading'
 import { getDailyCard } from '../../utilities/apiCalls'
 import { images, icons } from '../../utilities/images'
+import { Link } from 'react-router-dom'
 import './CardOfDay.css'
 
 const CardOfDay = ({ loading }) => {
@@ -26,7 +27,7 @@ const CardOfDay = ({ loading }) => {
   return (
     <div className="cod-ctr"><h2 className="cod-title">Card of the Day</h2>
     {!card ? <Loading image={icons[10]} message="Summoning card of the day..." /> : <div className="card-of-day-ctr">
-    <img className="cod" src={images.find(image => image.includes(card.name_short))} alt={card.name} />
+    <Link to={`/tarot/${card.name_short}`}><img className="cod" src={images.find(image => image.includes(card.name_short))} alt={card.name} /></Link>
         <p className="cod-name">{card.name}</p>
         <p className="cod-desc">{card.description}</p>
         <p>Check again tomorrow for a new reading!</p>
