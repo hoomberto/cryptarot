2c93f9256cc42b1ff2a62573c89ac4fabab79010
Bobby Vasquez <shootbv@gmail.com>
2021-08-08 18:07:29 -0600

    Remove commented out code from components

--- src/components/Btn/Btn.js
+++ src/components/Btn/Btn.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-// import { icons } from '../../utilities/images'
 import './Btn.css'
 
 
