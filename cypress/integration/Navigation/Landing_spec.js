de110edb11b809eeab6ffebd9b856bd2c343e46a
Bobby Vasquez <shootbv@gmail.com>
2021-08-09 11:31:36 -0600

    Add fixture for nomics data

--- cypress/integration/Navigation/Landing_spec.js
+++ cypress/integration/Navigation/Landing_spec.js
@@ -2,12 +2,12 @@ describe('Landing Page', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:3000');
-    cy.intercept("https://powerful-fjord-58193.herokuapp.com/https://api.nomics.com/v1/currencies/ticker?key=39882e2a6e59afede70e88be7f53e7d7ed14b4c0")
-    cy.intercept('https://cryptarot-api.herokuapp.com/api/v1/cards')
-    cy.intercept('https://cryptarot-api.herokuapp.com/api/v1/daily')
-    cy.intercept('https://cryptarot-api.herokuapp.com/api/v1/results')
+    cy.intercept("https://powerful-fjord-58193.herokuapp.com/https://api.nomics.com/v1/currencies/ticker?key=39882e2a6e59afede70e88be7f53e7d7ed14b4c0", {fixture: 'cryptos.json'})
+    cy.intercept('https://cryptarot-api.herokuapp.com/api/v1/cards', {fixture: 'cards.json'})
+    cy.intercept('https://cryptarot-api.herokuapp.com/api/v1/daily', {fixture: 'cardOfDay.json'})
+    cy.intercept('https://cryptarot-api.herokuapp.com/api/v1/results', {fixture: 'results.json'})
   });
-  
+
   it('User should see a header with the name of the app upon page load', () => {
     cy.get('header').contains('CRYPTAROT')
   })
