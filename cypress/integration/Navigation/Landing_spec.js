4f61beed400ede9ee8fb4b2606cec14649cfc777
Bobby Vasquez <shootbv@gmail.com>
2021-08-09 12:33:15 -0600

    Add test for navigating from landing page to other components

--- cypress/integration/Navigation/Landing_spec.js
+++ cypress/integration/Navigation/Landing_spec.js
@@ -1,20 +1,37 @@
-describe('Landing Page', () => {
+describe('Landing Page Navigation', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:3000');
     cy.intercept("https://powerful-fjord-58193.herokuapp.com/https://api.nomics.com/v1/currencies/ticker?key=39882e2a6e59afede70e88be7f53e7d7ed14b4c0", {fixture: 'cryptos.json'})
     cy.intercept('https://cryptarot-api.herokuapp.com/api/v1/cards', {fixture: 'cards.json'})
     cy.intercept('https://cryptarot-api.herokuapp.com/api/v1/daily', {fixture: 'cardOfDay.json'})
     cy.intercept('https://cryptarot-api.herokuapp.com/api/v1/results', {fixture: 'results.json'})
+    cy.visit('http://localhost:3000');
   });
 
-  it('User should see a header with the name of the app upon page load', () => {
-    cy.get('header').contains('CRYPTAROT')
+  it('User should be able to click "Get a Reading" link in header to navigate to card reading page', () => {
+    cy.navBarGoTo(1, 'Get a Reading')
+    cy.url().should('contain', '/pick')
   })
 
-  it('User should see navigation links in header that takes them to various site routes', () => {
-    cy.get('header').get('a:nth-of-type(1)').contains('Get a Reading')
-    cy.get('header').get('a:nth-of-type(2)').contains('View Cryptos')
-    cy.get('header').get('a:nth-of-type(3)').contains('View Tarot')
+  it('User should be able to click "View Cryptos" link in header to navigate to cryptos page', () => {
+    cy.navBarGoTo(2, 'View Cryptos')
+    cy.url().should('contain', '/cryptos')
+  })
+
+  it('User should be able to click "View Tarot" link in header to navigate to card tarot page', () => {
+    cy.navBarGoTo(3, 'View Tarot')
+    cy.url().should('contain', '/tarot')
+  })
+
+  it('User should be able to return back to landing page after clicking a link and going back in browser', () => {
+    let paths = {
+      '1': 'Get a Reading',
+      '2': 'View Cryptos',
+      '3': 'View Tarot'
+    }
+    for (let key in paths) {
+      cy.navBarGoTo(key, paths[key]).go('back')
+    }
+    cy.url().should('eq', 'http://localhost:3000/')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('navBarGoTo', (ofType, shouldContain) => {
+  cy.get('header').get(`a:nth-of-type(${ofType})`).contains(shouldContain).click()
+})
